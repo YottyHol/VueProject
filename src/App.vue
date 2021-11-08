c0d8ab7c36967ba70a2b14f7cef98401f5be505e
YottyHol <git@oholliday.co.uk>
2021-11-08 23:58:10 +0000

    primevue

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,59 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <div class="app-container">
+
+      <HelloWorld msg="Welcome to Your PrimeVue App"/>
+      <form @submit.prevent="greet">
+        <InputText type="text" v-model="text"/>
+        <Button type="submit" label="Submit"/>
+        <h3>{{message}}</h3>
+      </form>
+    </div>
+
+    <Toast/>
+  </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    data() {
+        return {
+            message: null,
+            text: null
+        }
+    },
+    methods: {
+        greet() {
+            this.$toast.add({severity: 'info', summary: 'Hello '  + this.text});
+            this.message = 'Hello ' + this.text;
+        }
+    },
+    components: {
+      HelloWorld
+    }
 }
 </script>
 
-<style>
+<style scoped>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.app-container {
+  text-align: center;
+}
+
+body #app .p-button {
+  margin-left: .2em;
+}
+
+form {
+  margin-top: 2em;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,25 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import App from './App.vue';
 
-createApp(App).mount('#app')
+import PrimeVue from 'primevue/config';
+import InputText from 'primevue/inputtext';
+import Button from 'primevue/button';
+import Toast from 'primevue/toast';
+import ToastService from 'primevue/toastservice';
+import Chart from 'primevue/chart';
+import './index.css'
+
+import 'primevue/resources/themes/vela-purple/theme.css';
+import 'primevue/resources/primevue.min.css';
+import 'primeicons/primeicons.css';
+
+const app = createApp(App);
+
+app.use(PrimeVue);
+app.use(ToastService);
+
+app.component('InputText', InputText);
+app.component('Button', Button);
+app.component('Toast', Toast);
+app.component('Chart', Chart);
+app.mount('#app')
